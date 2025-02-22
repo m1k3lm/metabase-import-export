75546a5217e81b17a2ecc72019b868410a47a7e8
Mikel Martin <mikel.martin@sequra.es>
2025-02-22 02:12:28 +0100

    Backup

--- scripts/export_metabase.py
+++ scripts/export_metabase.py
@@ -1,46 +1,47 @@
 import requests
 import json
 import os
+from dotenv import load_dotenv
 
-METABASE_HOST = os.getenv('METABASE_HOST')
-METABASE_USERNAME = os.getenv('METABASE_USERNAME')
-METABASE_PASSWORD = os.getenv('METABASE_PASSWORD')
+load_dotenv()
 
-def authenticate():
-    response = requests.post(f'{METABASE_HOST}/api/session', json={
-        'username': METABASE_USERNAME,
-        'password': METABASE_PASSWORD
-    })
-    response.raise_for_status()
-    return response.json()['id']
+METABASE_PORT = os.getenv('METABASE_PORT')
+METABASE_HOST = 'http://127.0.0.1:' + METABASE_PORT
+METABASE_API_KEY = os.getenv('METABASE_API_KEY')
 
 def export_metabase_content():
-    session_id = authenticate()
     headers = {
         'Content-Type': 'application/json',
-        'X-Metabase-Session': session_id
+        'x-api-key': METABASE_API_KEY
     }
 
-    # Export questions
-    questions_response = requests.get(f'{METABASE_HOST}/api/card', headers=headers)
-    questions_response.raise_for_status()
-    questions = questions_response.json()
-
-    # Export dashboards
-    dashboards_response = requests.get(f'{METABASE_HOST}/api/dashboard', headers=headers)
-    dashboards_response.raise_for_status()
-    dashboards = dashboards_response.json()
+    # # Export questions
+    # questions_response = requests.get(f'{METABASE_HOST}/api/card', headers=headers)
+    # questions_response.raise_for_status()
+    # questions = questions_response.json()
+    # # Save to file
+    # with open('metabase_export_questions.json', 'w') as f:
+    #     json.dump({
+    #         'questions': questions
+    #     }, f, indent=4)
+        
+    # # Export dashboards
+    # dashboards_response = requests.get(f'{METABASE_HOST}/api/dashboard', headers=headers)
+    # dashboards_response.raise_for_status()
+    # dashboards = dashboards_response.json()
+    # # Save to file
+    # with open('metabase_export_dashboards.json', 'w') as f:
+    #     json.dump({
+    #         'dashboards': questions
+    #     }, f, indent=4)
 
     # Export queries
-    queries_response = requests.get(f'{METABASE_HOST}/api/native-query', headers=headers)
+    queries_response = requests.get(f'{METABASE_HOST}/api/native-query-snippet', headers=headers)
     queries_response.raise_for_status()
     queries = queries_response.json()
-
     # Save to file
-    with open('metabase_export.json', 'w') as f:
+    with open('metabase_export_query_snippets.json', 'w') as f:
         json.dump({
-            'questions': questions,
-            'dashboards': dashboards,
             'queries': queries
         }, f, indent=4)
 
